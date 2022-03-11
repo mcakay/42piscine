2bbb6ecd96ba45f72c071838b9c2941749973151
mcakay <melihcakay@gmail.com>
2022-03-11 21:07:43 +0300

    reverse_bits fixed

--- exam/reverse_bits/reverse_bits.c
+++ exam/reverse_bits/reverse_bits.c
@@ -6,13 +6,16 @@
 /*   By: mcakay <melihcakay@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 20:29:47 by mcakay            #+#    #+#             */
-/*   Updated: 2022/03/09 20:48:57 by mcakay           ###   ########.fr       */
+/*   Updated: 2022/03/11 21:05:53 by mcakay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 unsigned char	reverse_bits(unsigned char octet)
 {
-	int			i;
+	int				i;
 	unsigned char	c;
 
 	i = 8;
@@ -23,5 +26,13 @@ unsigned char	reverse_bits(unsigned char octet)
 		octet /= 2;
 		i--;
 	}
-	return (c);	
-}	
+	return (c);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("%d", reverse_bits(atoi(argv[1])));
+	printf("\n");
+	return (0);
+}
