29c9324cc92890e2c78bc55cbeb2e86c7427fd7c
mcakay <melihcakay@gmail.com>
2022-03-11 20:59:37 +0300

    ft_range updated

--- exam/ft_range/ft_range.c
+++ exam/ft_range/ft_range.c
@@ -6,13 +6,13 @@
 /*   By: mcakay <melihcakay@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 22:07:39 by mcakay            #+#    #+#             */
-/*   Updated: 2022/03/10 22:10:53 by mcakay           ###   ########.fr       */
+/*   Updated: 2022/03/11 20:44:34 by mcakay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	abs(int x)
+int	ft_abs(int x)
 {
 	if (x < 0)
 		return (-x);
@@ -21,4 +21,29 @@ int	abs(int x)
 
 int	*ft_range(int start, int end)
 {
-	
+	int	i;
+	int	*tab;
+
+	i = 0;
+	tab = (int *)malloc(sizeof(int) * (ft_abs(start - end) + 1));
+	if (start < end)
+	{
+		while (start < end)
+		{
+			tab[i] = start;
+			start++;
+			i++;
+		}
+	}
+	else if (start > end)
+	{
+		while (start > end)
+		{
+			tab[i] = start;
+			start--;
+			i++;
+		}
+	}
+	tab[i] = start;
+	return (tab);
+}
